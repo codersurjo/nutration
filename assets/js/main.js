48d50b831f519d2fbdf11cb92364b8ea9597466a
codersurjo <codersurjo@gmail.com>
2021-07-07 20:16:36 +0600

    Slider add

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,22 @@ jQuery(document).ready(function() {
     jQuery('.menu__btn').click(function() {
         jQuery('.menu_area_design').slideToggle();
     });
+    jQuery(window).scroll(function() {
+        var scroll = $(window).scrollTop();
 
-    jQuery(".new_img1").owlCarousel({
+        //>=, not <=
+        if (scroll >= 10) {
+            //clearHeader, not clearheader - caps H
+            $(".header_area_design").addClass("header_area_design1");
+        }
+    })
+
+    jQuery(".banner_area_design").owlCarousel({
         autoplay: true,
         loop: true,
         margin: 0,
-        // animateOut: 'fadeOut',
-        // animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
         responsiveClass: true,
         autoplayTimeout: 2000,
         smartSpeed: 800,
