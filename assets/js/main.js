89456b850d8a2dbacbf77ade36673de9b6a05383
codersurjo <codersurjo@gmail.com>
2021-07-07 19:03:11 +0600

    Slider add

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,28 @@ jQuery(document).ready(function() {
         jQuery('.menu_area_design').slideToggle();
     });
 
+    jQuery(".new_img1").owlCarousel({
+        autoplay: true,
+        loop: true,
+        margin: 0,
+        // animateOut: 'fadeOut',
+        // animateIn: 'fadeIn',
+        responsiveClass: true,
+        autoplayTimeout: 7000,
+        smartSpeed: 800,
+        nav: false,
+        dots: false,
+        items: 1,
+    });
+
     // progressbar js 
     // progress bar js 
     const progress = document.querySelector('.progress-done');
     progress.style.width = progress.getAttribute('data-done') + '%';
     progress.style.opacity = 1;
+
+
+
 
 
 });
